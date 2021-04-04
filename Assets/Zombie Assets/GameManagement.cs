29ba17fd2d2b2efacb25802280c7c922535b4431
Gurjot Singh <deolgurjot68@gmail.com>
2021-04-04 02:18:36 -0700

    add

---
+++ Assets/Zombie Assets/GameManagement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviour
+{
+
+    public int round = 1;
+    int zombiesInRound = 10;
+    int zombiesSpawnedInRound = 0;
+    float zombiesSpwanTimer = 0;
+    public Transform[] zombieSpawnPoints;
+    public GameObject zombieEnemy;
+
+
+    void Update()
+    {
+     if(zombiesSpawnedInRound < zombiesInRound)
+        {
+            if(zombiesSpawnedInRound > 3)
+            {
+                SpawnZombie();
+                zombiesSpwanTimer = 0;
+            }
+            else 
+            {
+                zombiesSpwanTimer+= Time.deltaTime;
+            }
+        }
+
+     void SpawnZombie()
+        {
+            Vector3 randomSpwanPoint = zombieSpawnPoints[Random.Range(0, zombieSpawnPoints.Length)].position;
+            Instantiate(zombieEnemy, randomSpwanPoint, Quaternion.identity);
+            zombiesSpawnedInRound++;
+        
+        }
+    }
+}
