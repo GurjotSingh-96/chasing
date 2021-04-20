b6dd99c6c591d64116ddea7918e668cdfdea68d4
Gurjot Singh <deolgurjot68@gmail.com>
2021-04-19 19:41:17 -0700

    push

--- Assets/Zombie Assets/ZombieAttack.cs
+++ Assets/Zombie Assets/ZombieAttack.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class ZombieAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject ThePlayer;
+    public float TargetDistance;
+    public float AllowedRange = 10;
+    public GameObject TheEnemy;
+    public float EnemySpeed;
+    public int AttackTrigger;
+    public RaycastHit Shot;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(ThePlayer.transform);
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot))
+        {
+            TargetDistance = Shot.distance;
+            if (TargetDistance < AllowedRange)
+            {
+                EnemySpeed = 0.01f;
+                if (AttackTrigger == 0)
+                {
+                    TheEnemy.GetComponent<Animation>().Play("Walking");
+                    transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
+                }
+            }
+            else
+            {
+                EnemySpeed = 0;
+                TheEnemy.GetComponent<Animation>().Play("Idle");
+            }
+        }
+
+        if (AttackTrigger == 1)
+        {
+            EnemySpeed = 0;
+            TheEnemy.GetComponent<Animation>().Play("Attacking");
+        }
     }
+
+    void OnTriggerEnter()
+    {
+        AttackTrigger = 1;
+    }
+
+    void OnTriggerExit()
+    {
+        AttackTrigger = 0;
+    }
+
 }
