cb4a60b8357e49f4d6effa48c2bc5e7de4cd257f
GurjotSingh-96 <deolgurjot68@gmail.com>
2021-04-19 20:17:42 -0700

    push

---
+++ Assets/Zombie Assets/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+ {
+     public Transform playerTr;
+     private Transform zombieTr;
+     public float distance = 3.0f;
+     public float height = 3.0f;
+     public float damping = 5.0f;
+     private Vector3 followPosition;
+ 
+     void Awake()
+     {
+         zombieTr = this.transform;
+     }
+     
+     void Update () 
+     {
+        Follow();
+     }
+      
+     private void Follow()
+     {
+         //Transforms the position x, y, z from local space to world space.
+         followPosition = playerTr.TransformPoint(0, height, -distance);
+         //Lerp the zombie towards the followPosition.
+         zombieTr.position = Vector3.Lerp (zombieTr.position, followPosition, Time.deltaTime * damping);
+      }
+ }
